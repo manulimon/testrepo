7471f1613655a4fc2f160f8197a317097cf34ea8
manuligit <riina.vaisanen@gmail.com>
2017-03-27 15:57:40 +0300

    Failing tests

--- DummyTests/DummyTests.swift
+++ DummyTests/DummyTests.swift
@@ -23,6 +23,7 @@ class DummyTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1, 2);
     }
     
     func testPerformanceExample() {
